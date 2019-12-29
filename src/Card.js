1f8986533a12e9cfa31d4efaacfa8ba14613b4d1
Qaiyyum Hakimi <qaiyyumhakimi@gmail.com>
2019-12-29 11:17:03 +0800

    add card stories

---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Card = () => {
+    return (
+        <div style={{ padding: '10px', border: '1px solid black' }}>
+            This is card component
+        </div>
+    )
+}
